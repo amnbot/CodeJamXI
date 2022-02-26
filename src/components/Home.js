45be7dc6d06d71880831cc3a571f9ed61e578519
RadhSlayer <hirose546@gmail.com>
2022-02-26 14:14:59 -0500

    Search by tags

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,28 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase-config";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Scene from "./scenes/Scene";
+import { tags } from "../constants/constants";
 
 export default function Home() {
+  const [storyTags, setStoryTags] = useState(tags);
   const [sceneList, setSceneList] = useState([]);
   const [search, setSearch] = useState("");
+
+
+  const onCheckChange = (e) =>{
+    console.log(e.target)
+    
+    const temp = [...storyTags];
+
+    for (let i = 0; i < temp.length; i++) {
+      
+      if(temp[i].tag == e.target.id){
+        temp[i].value = Boolean(e.target.checked);
+      }
+    }
+    setStoryTags(temp);
+    //console.log(storyTags);
+  }
 
   const filterSearch = async () => {
     let scenes = [];
@@ -22,8 +40,23 @@ export default function Home() {
       scenes.push({ id: doc.id, ...doc.data() });
     });
 
+    
+
+    const q2 = query(
+      collection(db, "scenes"),
+      where("tags", "array-contains-any", storyTags)
+    );
+
+    const querySnapshot2 = await getDocs(q2);
+
+    querySnapshot2.forEach((doc) => {
+      if(!scenes.includes({ id: doc.id, ...doc.data() })){
+        scenes.push({ id: doc.id, ...doc.data() });
+      }
+    })
     setSceneList(scenes);
   };
+  
 
   const handleInput = (e) => {
     setSearch(e.target.value);
@@ -53,10 +86,19 @@ export default function Home() {
 
       <input
         onChange={handleInput}
-        placeholder="Scene Title"
+        placeholder="Search by name or tag..."
         maxLength="100" 
         className="m-auto mt-2.5 mb-2.5 border-black px-3 py-4 flex text-black bg-white rounded text-base border-0 shadow w-1/2"
       />
+      <div className="flex flex-row justify-center ">
+        <ul>
+          {storyTags.map((tag) => ( 
+            <li>
+              <input className="text-black" type="checkbox" id={tag.tag} onChange={onCheckChange} value={tag.value}/>{tag.tag}
+            </li>
+          ))}
+        </ul>
+        </div>
 
       <button className="flex mb-2.5 bg-green-600 rounded-lg w-1/5 text-white justify-center" onClick={filterSearch}>Search</button>
       <div>{sceneList.length > 0 ? displayScenes() : null}</div>
