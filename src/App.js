ba0d35c81f712e15b566544dfd83414f1dca1f49
AMNbot <aymenouali01@gmail.com>
2022-02-25 19:25:49 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="text-xl">XGZGEG</p>
         <a
           className="App-link"
           href="https://reactjs.org"
