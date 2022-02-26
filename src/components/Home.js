8e95ec874c6d4a84474e4909c3d022a00906a72d
AMNbot <aymenouali01@gmail.com>
2022-02-26 02:17:33 -0500

    Routes pass props (State)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,65 +1,61 @@
-import React, {useState} from "react";
-import {collection, getDocs, query, where} from "firebase/firestore"
+import React, { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase-config";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Scene from "./scenes/Scene";
 
 export default function Home() {
-  
   const [sceneList, setSceneList] = useState([]);
-  const[search,setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
-  const filterSearch = async () =>{
-    let scenes = []
-    console.log("click!");
-    const q = query(collection(db, "scenes"), where("title", "array-contains-any", search.split(" ")));
-    const querySnapshot = await getDocs(q)
+  const filterSearch = async () => {
+    let scenes = [];
+    const q = query(
+      collection(db, "scenes"),
+      where("title", "array-contains-any", search.split(" "))
+    );
+    const querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc) => {
       //console.log(doc.data(), doc.id);
 
-      scenes.push({id: doc.id, ...doc.data()});
-    })
+      scenes.push({ id: doc.id, ...doc.data() });
+    });
 
     setSceneList(scenes);
-  }       
+  };
 
-  const handleInput = (e) =>{
+  const handleInput = (e) => {
     setSearch(e.target.value);
-  }
+  };
 
-  const displayScenes = () =>{
-    return(
+  const displayScenes = () => {
+    return (
       <div>
-
-      <ul>
-        {sceneList.map(scene =>(
-
-          <li>
-            <Link to={`/scene/:${scene.id}`}>{scene.id}</Link>
-          </li>
+        <ul>
+          {sceneList.map((scene) => (
+            <li>
+              <Link to={`/scene/${scene.id}`} state={{ scene: scene }}>
+                {scene.id}
+              </Link>
+            </li>
           ))}
-      </ul>
-    
-
+        </ul>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
+      <input
+        onChange={handleInput}
+        placeholder="Scene Title"
+        maxLength="100"
+        className="m-auto mb-2.5 border-black px-3 py-4 flex text-black bg-white rounded text-base border-0 shadow w-1/2"
+      />
 
-    <input 
-    onChange={handleInput}
-    placeholder="Scene Title"
-    maxLength="100"
-    className="m-auto mb-2.5 border-black px-3 py-4 flex text-black bg-white rounded text-base border-0 shadow w-1/2"/>
-    
-    <button onClick={filterSearch}>Search</button>
-    <div>
-      {sceneList.length >0 ? displayScenes(): null}
+      <button onClick={filterSearch}>Search</button>
+      <div>{sceneList.length > 0 ? displayScenes() : null}</div>
     </div>
-    </div>
-    );
-
+  );
 }
--- src/components/scenes/Scene.js
+++ src/components/scenes/Scene.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams, useLocation } from "react-router-dom";
 
 export default function Scene() {
-    let { id }= useParams;
-    console.log(id);
-    return (
+  let { id } = useParams();
+  let location = useLocation();
+  const { scene } = location.state;
+  console.log(id, scene);
+
+  return (
     <div>
-        <h3>{id}</h3>
+      <h3>{id}</h3>
     </div>
-  )
+  );
 }
